3f1693af872a40c8abee31693c1f946945ec0b42
meeseLoL <107075158+meeseLoL@users.noreply.github.com>
2022-08-12 15:49:31 -0700

    Updates

---
+++ precache-manifest.d535aed05baeba175596608745436481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29712a4b24175b7e1e17953f6a4590f",
+    "url": "/JbDevDes/index.html"
+  },
+  {
+    "revision": "1eee422470656d5c7d8e",
+    "url": "/JbDevDes/static/css/main.9ee91a79.chunk.css"
+  },
+  {
+    "revision": "7510614ae394d5f34634",
+    "url": "/JbDevDes/static/js/2.4aadedc0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/JbDevDes/static/js/2.4aadedc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eee422470656d5c7d8e",
+    "url": "/JbDevDes/static/js/main.bc7e495e.chunk.js"
+  },
+  {
+    "revision": "a9af8ac723091ea6f859",
+    "url": "/JbDevDes/static/js/runtime-main.4b77d686.js"
+  }
+]);
